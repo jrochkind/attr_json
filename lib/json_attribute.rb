622557950be87e8ef8a56acd337918733275051d
Jonathan Rochkind <jonathan@dnil.net>
2017-04-27 17:38:33 -0400

    rspec

--- lib/json_attribute.rb
+++ lib/json_attribute.rb
@@ -1,5 +1,9 @@
 require "json_attribute/version"
 
+require "active_record"
+require "active_record/connection_adapters/postgresql_adapter"
+
+
 module JsonAttribute
   # Your code goes here...
 end
