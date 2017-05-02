18cf7a896b457481ff35800f8f8d92aedd6c3e3f
Jonathan Rochkind <jonathan@dnil.net>
2017-05-01 20:40:14 -0400

    change reader name in Type::Model

--- lib/json_attribute/type/model.rb
+++ lib/json_attribute/type/model.rb
@@ -8,29 +8,29 @@ module JsonAttribute
     # but normally that's only done in JsonAttribute::Model.to_type, there isn't
     # an anticipated need to create from any other place.
     class Model < ::ActiveModel::Type::Value
-      attr_accessor :json_hashable
-      def initialize(json_hashable)
-        #TODO type check, it really better be a json_hashable. maybe?
-        @json_hashable = json_hashable
+      attr_accessor :model
+      def initialize(model)
+        #TODO type check, it really better be a JsonAttribute::Model. maybe?
+        @model = model
       end
 
       def type
-        json_hashable.to_param.underscore.to_sym
+        model.to_param.underscore.to_sym
       end
 
       def cast(v)
         if v.nil?
           # TODO should we insist on an empty hash instead?
           v
-        elsif v.kind_of? json_hashable
+        elsif v.kind_of? model
           v
         elsif v.respond_to?(:to_hash)
           # to_hash is actually the 'implicit' conversion, it really is a hash
           # even though it isn't is_a?(Hash), try to_hash first before to_h,
           # the explicit conversion.
-          json_hashable.new(v.to_hash)
+          model.new(v.to_hash)
         elsif v.respond_to?(:to_h)
-          json_hashable.new(v.to_h)
+          model.new(v.to_h)
         else
           # Bad input? Most existing ActiveModel::Types seem to decide
           # either nil, or a base value like the empty string. They don't
@@ -42,7 +42,7 @@ module JsonAttribute
       def serialize(v)
         if v.nil?
           nil
-        elsif v.kind_of?(json_hashable)
+        elsif v.kind_of?(model)
           v.serializable_hash
         else
           cast(v).serializable_hash
