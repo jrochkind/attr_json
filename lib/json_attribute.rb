2e355e72ee65e4c831cc920794e3af053e18b659
Jonathan Rochkind <jonathan@dnil.net>
2018-03-24 12:47:59 -0400

    Don't test Dirty on Rails 5.0, it's not supported there
    Don't even "require" Dirty module on Rails 5.0

--- lib/json_attribute.rb
+++ lib/json_attribute.rb
@@ -6,8 +6,12 @@ require "active_record/connection_adapters/postgresql_adapter"
 require 'json_attribute/record'
 require 'json_attribute/model'
 require 'json_attribute/record/query_scopes'
-require 'json_attribute/record/dirty'
+
+# Dirty not supported on Rails 5.0
+if Gem.loaded_specs["activerecord"].version.release >= Gem::Version.new('5.1')
+  require 'json_attribute/record/dirty'
+end
 
 module JsonAttribute
-  # Your code goes here...
+
 end
