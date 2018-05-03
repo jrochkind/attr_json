d1c5d42579a242bb2294af7589a7f05c9a2fefcd
Jonathan Rochkind <jonathan@dnil.net>
2018-05-03 16:20:05 -0400

    polymorphic model type

---
+++ lib/attr_json/type/polymorphic_model.rb
@@ -0,0 +1,171 @@
+module AttrJson
+  module Type
+    # AttrJson::Type::PolymorphicModel can be used to create attr_json attributes
+    # that can hold any of various specified AttrJson::Model models. It is a
+    # _somewhat_ experimental feature.
+    #
+    # "polymorphic" may not be quite the right word, but we use it out of analogy
+    # with ActiveRecord [polymorphic assocications](http://guides.rubyonrails.org/association_basics.html#polymorphic-associations),
+    # which it resembles, as well as ActiveRecord [Single-Table Inheritance](http://guides.rubyonrails.org/association_basics.html#single-table-inheritance).
+    #
+    # Similar to these AR features, a PolymorphicModel-typed attribute will serialize the
+    # _model name_ of a given value in a `type` json hash key, so it can deserialize
+    # to the same correct model class.
+    #
+    # It can be used for single-model attributes, or arrays (which can be hetereogenous),
+    # in either AttrJson::Record or nested AttrJson::Models. If `CD`, `Book`, `Person`,
+    # and `Corporation` are all AttrJson::Model classes:
+    #
+    #      attr_json :favorite, AttrJson::Type::PolymorphicAttribute.new(CD, Book)
+    #      attr_json :authors, AttrJson::Type::PolymorphicAttribute.new(Person, Corporation), array: true
+    #
+    # Currently, you need a specific enumerated list of allowed types, and they all
+    # need to be AttrJson::Model classes. You can't at the moment have an "open" polymorphic
+    # type that can accept any AttrJson::Model.
+    #
+    # You can change the json key that the "type" (class name) for a value is stored to,
+    # when creating the type:
+    #
+    #      attr_json, :author, AttrJson::Type::PolymorphicAttribute.new(Person, Corporation, type_key: "__type__")
+    #
+    # But if you already have existing data in the db, that's gonna be problematic to change on the fly.
+    #
+    # You can set attributes with a hash, but it needs to have an appropriate `type` key
+    # (or other as set by `type_key` arg). If it does not, or you try to set a non-hash
+    # value, you will get a AttrJson::Type::PolymorphicModel::TypeError. (maybe a validation
+    # error would be better? but it's not what it does now.)
+    #
+    # **Note** this
+    # also applies to loading non-compliant data from the database. If you have non-compliant
+    # data in the db, the only way to look at it will be as a serialized json string in top-level
+    # {#json_attributes_before_cast} (or other relevant container attribute.)
+    #
+    # There is no built-in form support for PolymorphicModels, you'll have to work it out.
+    #
+    # ## jsonb_contains support
+    #
+    # There is basic jsonb_contains support, but no sophisticated type-casting like normal, beyond
+    # the polymorphic attribute. But you can do:
+    #
+    #      MyRecord.jsonb_contains(author: { name: "foo"})
+    #      MyRecord.jsonb_contains(author: { name: "foo", type: "Corporation"})
+    #      MyRecord.jsonb_contains(author: Corporation.new(name: "foo"))
+    #
+    class PolymorphicModel < ActiveModel::Type::Value
+      class TypeError < ::TypeError ; end
+
+      attr_reader :type_key, :unrecognized_type, :model_type_lookup
+      def initialize(*args)
+        options = { type_key: "type", unrecognized_type: :raise}.merge(
+          args.extract_options!.assert_valid_keys(:type_key, :unrecognized_type)
+        )
+        @type_key = options[:type_key]
+        @unrecognized_type = options[:unrecognized_type]
+
+        model_types = args
+
+        model_types.collect! do |m|
+          if m.respond_to?(:ancestors) && m.ancestors.include?(AttrJson::Model)
+            m.to_type
+          else
+            m
+          end
+        end
+
+        if bad_arg = model_types.find { |m| !m.is_a? AttrJson::Type::Model }
+          raise ArgumentError, "#{self.class.name} only works with AttrJson::Model / AttrJson::Type::Model, not '#{bad_arg.inspect}'"
+        end
+        if type_key_conflict = model_types.find { |m| m.model.attr_json_registry.has_attribute?(@type_key) }
+          raise ArgumentError, "conflict between type_key '#{@type_key}' and an existing attr_json in #{type_key_conflict.model}"
+        end
+
+        @model_type_lookup = model_types.collect do |type|
+          [type.model.name, type]
+        end.to_h
+      end
+
+      def model_names
+        model_type_lookup.keys
+      end
+
+      def model_types
+        model_type_lookup.values
+      end
+
+      # ActiveModel method, symbol type label
+      def type
+        @type ||= "any_of_#{model_types.collect(&:type).collect(&:to_s).join('_')}".to_sym
+      end
+
+      def cast(v)
+        if v.nil?
+          v
+        elsif model_names.include?(v.class.name)
+          v
+        elsif v.respond_to?(:to_hash)
+          cast_from_hash(v.to_hash)
+        elsif v.respond_to?(:to_h)
+          cast_from_hash(v.to_h)
+        else
+          raise_bad_model_name(v.class, v)
+        end
+      end
+
+      def serialize(v)
+        model_name = v.class.name
+        type = type_for_model_name(model_name)
+
+        raise_bad_model_name(model_name, v) if type.nil?
+
+        type.serialize(v).merge(type_key => model_name)
+      end
+
+      def deserialize(v)
+        cast(v)
+      end
+
+      def type_for_model_name(model_name)
+        model_type_lookup[model_name]
+      end
+
+      # This is used only by our own keypath-chaining query stuff.
+      # For PolymorphicModel type, it does no type casting, just
+      # sticks whatever you gave it in, which needs to be json-compat
+      # values.
+      def value_for_contains_query(key_path_arr, value)
+        hash_arg = {}
+        key_path_arr.each.with_index.inject(hash_arg) do |hash, (n, i)|
+          if i == key_path_arr.length - 1
+            hash[n] = value
+          else
+            hash[n] = {}
+          end
+        end
+        hash_arg
+      end
+
+      protected
+
+      def raise_missing_type_key(value)
+        raise TypeError, "AttrJson::Type::Polymorphic can't cast without '#{type_key}' key: #{value}"
+      end
+
+      def raise_bad_model_name(name, value)
+        raise TypeError, "This AttrJson::Type::PolymorphicType can only include {#{model_names.join(', ')}}, not '#{name}': #{value.inspect}"
+      end
+
+      def cast_from_hash(hash)
+        new_hash = hash.stringify_keys
+        model_name = new_hash.delete(type_key.to_s)
+
+        raise_missing_type_key(hash) if model_name.nil?
+
+        type = type_for_model_name(model_name)
+
+        raise_bad_model_name(model_name, hash) if type.nil?
+
+        type.cast(new_hash)
+      end
+    end
+  end
+end
---
+++ spec/type/polymorphic_type_spec.rb
@@ -0,0 +1,185 @@
+require 'spec_helper'
+
+RSpec.describe AttrJson::Type::PolymorphicModel do
+  let(:model1) do
+    Model1 = Class.new do
+      include AttrJson::Model
+      attr_json :str, :string
+      attr_json :int, :integer
+    end
+  end
+
+  let(:model2) do
+    Model2 = Class.new do
+      include AttrJson::Model
+      attr_json :str, :string
+      attr_json :bool, :boolean
+    end
+  end
+
+  let(:klass) do
+    # workaround to ruby scope weirdness
+    our_model1, our_model2 = model1, model2
+    TestRecord = Class.new(ActiveRecord::Base) do
+      include AttrJson::Record
+      include AttrJson::Record::QueryScopes
+
+      self.table_name = "products"
+
+      attr_json :one_poly, AttrJson::Type::PolymorphicModel.new(our_model1, our_model2)
+      attr_json :many_poly, AttrJson::Type::PolymorphicModel.new(our_model1, our_model2), array: true
+    end
+  end
+
+  # remove those constants we created for test classes
+  after do
+    Object.send(:remove_const, :Model1) if defined?(Model1)
+    Object.send(:remove_const, :Model2) if defined?(Model2)
+    Object.send(:remove_const, :TestRecord) if defined?(TestRecord)
+  end
+
+  let(:instance) { klass.new }
+
+  describe "conflicting type key" do
+    let(:model1) do
+      Model1 = Class.new do
+        include AttrJson::Model
+        attr_json :str, :string
+        attr_json :int, :integer
+        attr_json :type, :string
+      end
+    end
+
+    it "it raises" do
+      expect { klass }.to raise_error ArgumentError
+    end
+  end
+
+  describe "define with non-model type" do
+    let(:klass) do
+      # workaround to ruby scope weirdness
+      our_model1 = model1
+      TestRecord = Class.new(ActiveRecord::Base) do
+        include AttrJson::Record
+        self.table_name = "products"
+
+        attr_json :one_poly, AttrJson::Type::PolymorphicModel.new(our_model1, :string)
+      end
+    end
+
+    it "raises" do
+      expect { klass }.to raise_error ArgumentError
+    end
+  end
+
+  describe "single poly" do
+    it "sets and saves model" do
+      instance.one_poly = model1.new(str: "str", int: 12)
+      instance.save!
+      instance.reload
+
+      expect(instance.one_poly).to eq model1.new(str: "str", int: 12)
+    end
+    it "can set hash with type key" do
+      instance.one_poly = { str: "str", int: 12, type: "Model1" }
+      instance.save!
+      instance.reload
+
+      expect(instance.one_poly).to eq model1.new(str: "str", int: 12)
+    end
+
+    it "has a type name" do
+      expect(AttrJson::Type::PolymorphicModel.new(model1, model2).type).to eq :any_of_model1_model2
+    end
+  end
+
+  describe "array of poly" do
+    it "sets and saves models" do
+      instance.many_poly = [model1.new(str: "str", int: 12), model2.new(str: "str", bool: true)]
+      instance.save!
+      instance.reload
+
+      expect(instance.many_poly).to eq [model1.new(str: "str", int: 12), model2.new(str: "str", bool: true)]
+    end
+    it "can set hashes with type key" do
+      instance.many_poly = [{ str: "str", int: 12, type: "Model1" }, { str: "str", bool: true, type: "Model2" }]
+      instance.save!
+      instance.reload
+
+      expect(instance.many_poly).to eq [model1.new(str: "str", int: 12), model2.new(str: "str", bool: true)]
+    end
+  end
+
+  describe "bad types" do
+    it "raises on hash set with no type key" do
+      expect { instance.one_poly = { str: "str", int: 12 } }.to raise_error AttrJson::Type::PolymorphicModel::TypeError
+    end
+
+    it "raises on hash set with bad type key" do
+      expect { instance.one_poly = { str: "str", int: 12, type: "Nope" } }.to raise_error AttrJson::Type::PolymorphicModel::TypeError
+    end
+
+    it "raises on set with bad class" do
+      expect { instance.one_poly = "foo bar this is a string" }.to raise_error AttrJson::Type::PolymorphicModel::TypeError
+    end
+
+    it "raises on save for sneaky hash set with no type key" do
+      instance.json_attributes["one_poly"] = { str: "str", int: 12 }
+      expect { instance.save! }.to raise_error AttrJson::Type::PolymorphicModel::TypeError
+    end
+
+    it "raises on save for sneaky hash set with bad type key" do
+      instance.json_attributes["one_poly"] = { str: "str", int: 12, type: "Nope" }
+      expect { instance.save! }.to raise_error AttrJson::Type::PolymorphicModel::TypeError
+    end
+
+    it "raises on save for sneaky hash set to bad type" do
+      instance.json_attributes["one_poly"] = "this is a string not a hash"
+      expect { instance.save! }.to raise_error AttrJson::Type::PolymorphicModel::TypeError
+    end
+
+    describe "bad values in database" do
+      let (:bad_value) { {"one_poly" => "foo"} }
+      before do
+        # hard to get bad values in the db, heh
+        instance.save
+        ActiveRecord::Base.connection.execute("update products set json_attributes='#{bad_value.to_json}' where id=#{instance.id}")
+      end
+
+      it "can load without error" do
+        instance.reload
+      end
+
+      it "can show before_type_cast without error" do
+        instance.reload
+        expect(JSON.parse(instance.json_attributes_before_type_cast)).to eq bad_value
+      end
+
+      it "errors on accessing attribute" do
+        instance.reload
+        expect { instance.one_poly }.to raise_error AttrJson::Type::PolymorphicModel::TypeError
+      end
+
+      # Not so much intentional design, but this is what it does...
+      it "errors on accessing container" do
+        instance.reload
+        expect { instance.json_attributes }.to raise_error AttrJson::Type::PolymorphicModel::TypeError
+      end
+    end
+  end
+
+  describe "jsonb_contains" do
+    it "can create keypath query" do
+      sql = klass.jsonb_contains("one_poly.bool" => true).to_sql
+      expect(sql).to include "products.json_attributes @> ('{\"one_poly\":{\"bool\":true}}')"
+    end
+    it "can create keypath query with type" do
+      sql = klass.jsonb_contains("one_poly" => {"bool" => true, "type" => "Model2"}).to_sql
+      expect(sql).to include "products.json_attributes @> ('{\"one_poly\":{\"bool\":true,\"type\":\"Model2\"}}')"
+    end
+    it "can create keypath query with model arg" do
+      sql = klass.jsonb_contains("one_poly" => model2.new(bool: true)).to_sql
+      expect(sql).to include "products.json_attributes @> ('{\"one_poly\":{\"bool\":true,\"type\":\"Model2\"}}')"
+    end
+  end
+end
