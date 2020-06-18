92bdbcedd0269a0e7c108a1aad5ffdf27d50826a
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-06-18 16:15:43 -0400

    test multiple attributes, with and without defaults

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -786,13 +786,22 @@ RSpec.describe AttrJson::Record do
 
             self.table_name = "products"
             attr_json :str, :string, array: true, rails_attribute: true, default: 'foo'
+            attr_json :int, :integer, rails_attribute: true
           end
         end
         it "registers attribute and type" do
           expect(instance.attributes.keys).to include("str")
-          expect(instance.attributes["str"]).to eq ['foo']
           expect(instance.type_for_attribute("str")).to be_kind_of(AttrJson::Type::Array)
           expect(instance.type_for_attribute("str").base_type).to be_kind_of(ActiveModel::Type::String)
+
+        end
+
+        it "has initial values" do
+          expect(instance.attributes["str"]).to eq ['foo']
+          # this seems to be consistent with ordinary rails attribute use, not marked changed with initial default
+          expect(instance.str_changed?).to be(false)
+
+          expect(instance.attributes["int"]).to be_nil
           expect(instance.str_changed?).to be(false)
         end
 
@@ -801,10 +810,14 @@ RSpec.describe AttrJson::Record do
         end
 
         it 'syncs Rails attributes and default values after find' do
-          instance.save
+          instance.update(str: "our str", int: 100)
           found_record = klass.find(instance.id)
-          expect(found_record.attributes["str"]).to eq ['foo']
+
+          expect(found_record.attributes["str"]).to eq ['our str']
           expect(found_record.str_changed?).to be(false)
+
+          expect(found_record.attributes["int"]).to eq 100
+          expect(found_record.int_changed?).to be(false)
         end
       end
     end
